f5a8914f05373db7400a150adec60c7f7268c566
Lin Xia <lin.xia@nuance.com>
2018-09-08 21:53:24 +0800

    add test data

--- web/view/home.py
+++ web/view/home.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import logging.config
 import DeleteImage
 import AddImage
+import datetime
 
 logging.config.fileConfig('log.conf')
 logger = logging.getLogger('home')
@@ -15,15 +16,25 @@ def admin(request):
     pwd = request.GET.get('pwd')
     if "123" == pwd:
         return render(request, 'admin.html', None)
-    return render(request, 'error.html', None)
+    return render(request, 'admin.html', None)
 
 def view(request):
     return render(request, 'search.html', None)
 
 def delete(request):
+    starttime = datetime.datetime.now()
     DeleteImage.delete()
-    return render(request, 'success.html', None)
+    endtime = datetime.datetime.now()
+    addtime = (endtime-starttime).seconds
+    context = {}
+    context['addtime'] = addtime
+    return render(request, 'success.html', context)
 
 def add(request):
+    starttime = datetime.datetime.now()
     AddImage.import_image("images")
-    return render(request, 'success.html', None)
+    endtime = datetime.datetime.now()
+    addtime = (endtime-starttime).seconds
+    context = {}
+    context['addtime'] = addtime
+    return render(request, 'success.html', context)
