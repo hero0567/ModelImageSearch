f17d24e45b143207cde11cd337313498d2d93cbc
Lin Xia <lin.xia@nuance.com>
2018-09-10 22:28:22 +0800

    fix cannot load jpg error

--- AddImage.py
+++ AddImage.py
@@ -9,9 +9,11 @@ logger = logging.getLogger('AddImage')
 es = Elasticsearch()
 ses = SignatureES(es)
 count = 0
+error = 0
 
 def add_dir_image(src):
     global count
+    global error
     logger.info('Try to add images from %s', src)
     if os.path.isdir(src):
         files = os.listdir(src)
@@ -21,13 +23,19 @@ def add_dir_image(src):
                 continue
             else:
                 if file.lower().endswith(".jpg") or file.lower().endswith(".png"):
-                    ses.add_image(os.path.join(src, file))
+                    logger.info("Add image %s", os.path.join(src, file))
+                    try:
+                        ses.add_image(os.path.join(src, file))
+                    except:
+                        error = error + 1
+                        logger.error("Failed to add %s to server", os.path.join(src, file))
                     count=count+1
 
 
 def import_image(src):
     add_dir_image(src)
     logger.info("Total %s images added to server.", count)
+    logger.info("Total %s error images add failed to server.", error)
 
 if __name__ == '__main__':
     import_image(os.path.join("images"))
