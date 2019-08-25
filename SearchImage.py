b086f34de2e1bac1a5219929babe7802bc620a84
hero0567 <hero0567@163.com>
2019-08-25 21:49:08 +0800

    increase timeout to 30s

--- SearchImage.py
+++ SearchImage.py
@@ -7,14 +7,14 @@ logger = logging.getLogger('SearchImage')
 
 def search(name, all_orientations=False, image_match=0.6):
     es = Elasticsearch()
-    ses = SignatureES(es, size=15)
+    ses = SignatureES(es, size=15, timeout='30s')
     logger.info("Search image %s...", name)
     #result = ses.search_image(name)
     result = ses.search_image(name, all_orientations)
     size = len(result)
     if size == 0 and image_match != None:
         logger.info("No image found for %s", name)
-        ses = SignatureES(es, size=15, distance_cutoff=float(image_match))
+        ses = SignatureES(es, size=15, timeout='30s', distance_cutoff=float(image_match))
         result = ses.search_image(name, all_orientations)
         logger.info("Second distance_cutoff %s found %s image for %s", image_match, size, name)
     else:
